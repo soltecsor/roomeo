08ce00539ec387b9d4087e51a201edc27499a4c0
soltecsor <soltec.sor@gmail.com>
2020-05-24 15:36:41 -0300

    removendo linha api key

--- resources/views/portal/layouts/app.blade.php
+++ resources/views/portal/layouts/app.blade.php
@@ -191,9 +191,7 @@
     <script src="public/portal/assets/js/scripts.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.min.js"></script>
     <script src="{{asset('public/js').'/emailValidate.js'}}"></script>
-    <!-- <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=  &callback=initMap" type="text/javascript"></script>
-                             -->
+  
     <!-- <script src="{{asset('public/js/app.js')}}"></script> -->                          
     @include('portal.inc.svg')
     
